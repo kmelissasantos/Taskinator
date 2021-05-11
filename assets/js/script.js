17d112731c62fa75d43cca08e69b2d92f07cb894
Melissa Santos <melissasantos@melissas-mbp.attlocal.net>
2021-05-11 19:08:01 -0400

    updated task-form

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,6 +3,9 @@ console.log(buttonEl);
 
 var buttonEl = document.querySelector("#save-task");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
+var formEl = document.querySelector("#task-form");
+var tasksToDoEl = document.querySelector("#tasks-to-do");
+
 
 var createTaskHandler = function(event) {
     event.preventDefault();
